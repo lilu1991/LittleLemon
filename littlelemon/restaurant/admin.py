63899af241d0e77168f688d3ef8166f85441df29
Lu LI <luli@LudeMacBook-Pro.local>
2023-06-09 21:35:54 +0200

    Update

--- littlelemon/restaurant/admin.py
+++ littlelemon/restaurant/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Booking, Menu
+
 
 # Register your models here.
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ["name", "no_of_guests", "booking_date"]
+
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ["title", "price", "inventory"]
