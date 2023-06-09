a28a015b00608354283cbdd290842dd720b0251c
Lu LI <luli@LudeMacBook-Pro.local>
2023-06-09 22:09:21 +0200

    Update

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework import routers
 
-urlpatterns = [path("", views.index, name="index")]
+router = routers.DefaultRouter()
+router.register("tables", views.BookingViewSet, "booking")
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("menu/", views.MenuItemsView.as_view()),
+    path("menu/<int:pk>", views.SingleMenuItemView.as_view()),
+    path("booking/", include(router.urls)),
+]
